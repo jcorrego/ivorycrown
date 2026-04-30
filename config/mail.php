2afab8b5ae77e43aa851baacfc1c2d4fcbec25c5
Zoe <luke.bot.assistant@gmail.com>
2026-05-01 00:26:49 +0200

    feat: configure resend mailer

--- config/mail.php
+++ config/mail.php
@@ -115,6 +115,11 @@ return [
         'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
     ],
 
+    'reply_to' => [
+        'address' => env('MAIL_REPLY_TO_ADDRESS', env('MAIL_FROM_ADDRESS', 'hello@example.com')),
+        'name' => env('MAIL_REPLY_TO_NAME', env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel'))),
+    ],
+
     'club_applications_to' => env('CLUB_APPLICATIONS_MAIL_TO', env('MAIL_FROM_ADDRESS', 'hello@example.com')),
 
 ];
